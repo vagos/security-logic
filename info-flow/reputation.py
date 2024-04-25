619272f9715f453901e8b5368937f7994b1eca2b
Siyan <146075999+chensy618@users.noreply.github.com>
2024-04-25 14:39:50 +0200

    information flow analysis completed

--- info-flow/reputation.py
+++ info-flow/reputation.py
@@ -19,7 +19,8 @@ class AuctionHouse:
         self.customers[name] = customer_info  # self.customers[name] : {AuctionHouse : AuctionHouse}
 
     def set_auction_house_trust(self, auction_house_name, verification_method):
-        self.trusted_auction_houses[auction_house_name] = verification_method #verification_method : {AuctionHouse' : {AuctionHouse'}} all the customers (and their bid_limit) owned by other auction houses used as the reference
+        self.trusted_auction_houses[auction_house_name] = verification_method 
+        #verification_method : {AuctionHouse' : {AuctionHouse'}} all the customers (and their bid_limit) owned by other trusted auction houses used as the reference
         # auction_house_name : {AuctionHouse : {⊥}}, everyone can read auction house name
         # we use customer's bid limit as verification method, the bid limit : {AuctionHouse : {Customer,AuctionHouse}}, so auction house can read customer's bid limit
 
@@ -34,7 +35,7 @@ class AuctionHouse:
             return None # {AuctionHouse' : {AuctionHouse'}}
 
     def accept_customer_with_reference(self, customer_name, reference_auction_house_name):
-        # customer_name : {Customer : AuctionHouse}
+        # customer_name : {Customer : {Customer,AuctionHouse}}
         # reference_auction_house_name : {AuctionHouse' : {AuctionHouse'}}
         # self.trusted_auction_houses : {AuctionHouse' : {AuctionHouse'}}
         # verification_method : {AuctionHouse : {AuctionHouse}}
@@ -62,44 +63,38 @@ class AuctionHouse:
 
 def main():
 
-    # Suggestion : we may stand on only one auction house's perspective, and consider the information flow from the auction house's perspective
-    # For example, a trust c, a trust b, a trust d. now, we have b trust a, c trust b. 
-    # Because the data from other auction houses is not directly accessible, it would be more clear that which auction house is the main focus of the analysis.
-
     # Setup
 
-    auction_house_a = AuctionHouse("AuctionHouseA") # {AuctionHouse : {AuctionHouse,Customer}}
-    auction_house_b = AuctionHouse("AuctionHouseB") # {AuctionHouse : {AuctionHouse,Customer}}
-    auction_house_c = AuctionHouse("AuctionHouseC") # {AuctionHouse : {AuctionHouse,Customer}}
+    auction_house_a = AuctionHouse("AuctionHouseA") # {AuctionHouse : {Customer,AuctionHouse}}
+    auction_house_b = AuctionHouse("AuctionHouseB") # {AuctionHouse : {Customer,AuctionHouse}}
+    auction_house_c = AuctionHouse("AuctionHouseC") # {AuctionHouse : {Customer,AuctionHouse}}
 
-    # Consider declassfy the customer's name and bid limit. Customer provides the name and bid limit, auction house stores the customer's name and bid limit
-    auction_house_a.add_customer("Alice", 500) # {Customer : AuctionHouse}
+    auction_house_a.add_customer("Alice", 500) 
+    #customer name : {Customer : {Customer,AuctionHouse}},customer bid_limit : {Customer : {Customer,AuctionHouse}} 
 
+    # solved for now : can explain the report, and then delete the comment. 
     # With regard to the trust auction house, each auction house trusts the other auction house. 
     # Although the auction house name is public, the verification method (bid limit) is private.
     # And essentially, the auction house only knows their owned customer bid limit. therefore, we may also consider the declassification of bid limit from other auction houses
+    
     # B trusts A by specifying A's verification method
     #if_acts_for(main,auction_house_a)
     #verification := declassify(auction_house_a,{auction_house_n (these are any other auction houses)})
     verificaition = auction_house_a.verify_customer_bid_limit()
-    auction_house_b.set_auction_house_trust("AuctionHouseA", verificaition) # {AuctionHouse : {AuctionHouse}}
+    auction_house_b.set_auction_house_trust("AuctionHouseA", verificaition) # {AuctionHouse' : {AuctionHouse'}} auction house prime means other auction houses e.g. auction house b
 
     # C trusts B in a similar manner
-    auction_house_c.set_auction_house_trust("AuctionHouseB", auction_house_b.verify_customer_bid_limit) # {AuctionHouse : {AuctionHouse}}
+    auction_house_c.set_auction_house_trust("AuctionHouseB", auction_house_b.verify_customer_bid_limit) # {AuctionHouse' : {AuctionHouse'}}
 
     # Verifying Alice's bid limit through references
-
-    # This is run by the customer   Q: why is run by customer? 
+    # The following method is run by auction house prime
     # auction house b accepts the auction house a's customer, Alice, with the reference of auction house a
-    auction_house_b.accept_customer_with_reference("Alice", "AuctionHouseA") #"Alice" : {Customer : AuctionHouse}, "AuctionHouseA" : {AuctionHouse : {AuctionHouse}}
-    # auction house c accepts the auction house b's customer, Alice, with the reference of auction house b
-    auction_house_c.accept_customer_with_reference("Alice", "AuctionHouseB") #"Alice" : {Customer : AuctionHouse}, "AuctionHouseB" : {AuctionHouse : {AuctionHouse}}
-    # auction house b accepts the auction house a's customer, Bob, with the reference of auction house a
-    auction_house_b.accept_customer_with_reference("Bob", "AuctionHouseA")   #"Bob" : {Customer : AuctionHouse}, "AuctionHouseA" : {AuctionHouse : {AuctionHouse}}
+    auction_house_b.accept_customer_with_reference("Alice", "AuctionHouseA") 
+    auction_house_c.accept_customer_with_reference("Alice", "AuctionHouseB") 
+    auction_house_b.accept_customer_with_reference("Bob", "AuctionHouseA")
 
-    # auction house b prints the customer's reference from aution house a
+    # auction house prints their customer's reference
     auction_house_b.print_customers()   # {AuctionHouse : {AuctionHouse}}
-    # auction house c prints the customer's reference from aution house b
     auction_house_c.print_customers()   # {AuctionHouse : {AuctionHouse}}
 
 main()
